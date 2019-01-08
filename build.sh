18f5078e35c7583ef9ef7be84b40370403a0a285
Mattias Törnblad <mattias.tornblad@gmail.com>
2019-01-08 13:18:05 +0100

    Created X96 device

---
+++ build.sh
@@ -0,0 +1,55 @@
+# 64-bit x86 compatible device, e.g. Desktop PC, Intel NUC, ATOM
+#PROJECT=Generic ARCH=x86_64
+
+# Raspberry Pi zero or 1:
+#PROJECT=RPi DEVICE=RPi ARCH=arm
+
+# Raspberry Pi 2 or 3:
+#PROJECT=RPi DEVICE=RPi2 ARCH=arm
+
+# HardKernel Odroid C2
+#PROJECT=Amlogic DEVICE=Odroid_C2 ARCH=arm
+
+# LePotato
+#PROJECT=Amlogic DEVICE=LePotato ARCH=arm
+
+# S905
+#PROJECT=Amlogic DEVICE=S905 ARCH=arm
+
+# S912
+#PROJECT=Amlogic DEVICE=S912 ARCH=arm
+
+# WeTek Hub
+#PROJECT=Amlogic DEVICE=WeTek_Hub ARCH=arm
+
+# WeTek Play 2
+#PROJECT=Amlogic DEVICE=WeTek_Play_2 ARCH=arm
+
+# WeTek Play
+#PROJECT=WeTek_Play ARCH=arm
+
+# WeTek Core
+PROJECT=WeTek_Core ARCH=arm
+
+# Rockchip RK3328
+#PROJECT=Rockchip DEVICE=RK3328 ARCH=arm UBOOT_SYSTEM=box
+#PROJECT=Rockchip DEVICE=RK3328 ARCH=arm UBOOT_SYSTEM=box-trn9
+#PROJECT=Rockchip DEVICE=RK3328 ARCH=arm UBOOT_SYSTEM=box-z28
+#PROJECT=Rockchip DEVICE=RK3328 ARCH=arm UBOOT_SYSTEM=roc-cc
+#PROJECT=Rockchip DEVICE=RK3328 ARCH=arm UBOOT_SYSTEM=rock64
+#PROJECT=Rockchip DEVICE=RK3328 ARCH=arm UBOOT_SYSTEM=rockbox
+
+# Rockchip RK3288
+#PROJECT=Rockchip DEVICE=TinkerBoard ARCH=arm
+#PROJECT=Rockchip DEVICE=MiQi ARCH=arm
+
+# Rockchip RK3399
+#PROJECT=Rockchip DEVICE=RK3399 ARCH=arm UBOOT_SYSTEM=rock960
+#PROJECT=Rockchip DEVICE=RK3399 ARCH=arm UBOOT_SYSTEM=rockpro64
+#PROJECT=Rockchip DEVICE=RK3399 ARCH=arm UBOOT_SYSTEM=sapphire
+
+# Set up custom path for sources
+# SOURCES_DIR=/server/dev/sources
+
+
+make image
